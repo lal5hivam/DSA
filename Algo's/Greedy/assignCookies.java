28e6fe353e21c3353912ef39c433fdd73709d63b
lal5hivam <143792762+lal5hivam@users.noreply.github.com>
2025-03-12 22:05:07 +0530

    LeetCode(link)
    https://leetcode.com/problems/assign-cookies/submissions/1571553254/

---
+++ Algo's/Greedy/assignCookies.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int l = 0, r = 0;
+        int ans = 0;
+        while (l < g.length && r < s.length) {
+            if (s[r] >= g[l]) {
+                ans++;
+                l++;
+                r++;
+            } else {
+                r++;
+            }
+        }
+
+        return ans;
+    }
+}
